6a8d3a0e5d5006e33239b6dce96228dfb3cbe3c8
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-22 16:15:23 +0100

    automatically generates carousel indicators

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,6 @@
 $(function() {
 	var topoffset = 50; // variable for menu height
+	var slideqty = $('#featured .item').length;
 
 	$('body').scrollspy({
 		target: 'header .navbar',
@@ -19,6 +20,13 @@ $(function() {
 		}
 	});
 
+
+
+	//Automatically generate carousel indicators
+    for (var i=0; i < slideqty; i++) {
+        var insertText = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
+        $('#featured ol').append(insertText);
+    }
 
 
 	$('.carousel').carousel({
