d6c8c3752582e7760e609b3b064e8feb7f806098
Stefan Lissia <stefan.emailacct@gmail.com>
2015-09-22 17:31:50 +0100

    replaces img inside carousel with a fullscreen img with jquery

--- js/myscript.js
+++ js/myscript.js
@@ -1,6 +1,7 @@
 $(function() {
 	var topoffset = 50; // variable for menu height
 	var slideqty = $('#featured .item').length;
+	var wheight = $(window).height(); //gets the height of the window
 
 	$('body').scrollspy({
 		target: 'header .navbar',
@@ -27,6 +28,20 @@ $(function() {
         var insertText = '<li data-target="#featured" data-slide-to="' + i + '"></li>';
         $('#featured ol').append(insertText);
     }
+
+
+
+    $('.fullheight').css('height', wheight);
+
+
+   	//replace IMG inside carousels with a background image
+   	$('#featured .item img').each(function() {
+   		var imgSrc = $(this).attr('src');
+   		$(this).parent().css({'background-image': 'url(' + imgSrc + ')'});
+   		$(this).remove();	
+   	});
+
+
 
 
 	$('.carousel').carousel({
